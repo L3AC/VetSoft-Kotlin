93145442f8a1b1a068f90f9356da90e1aa4d914a
LUIS EDUARDO ALVARENGA CLAROS <20220298@ricaldone.edu.sv>
2023-05-27 21:14:32 -0600

    Cambio

---
+++ app/src/main/java/com/example/vetsoft/Conex/Conx.kt
@@ -0,0 +1,9 @@
+package com.example.vetsoft.Conex
+
+import android.util.Log
+
+class conx {
+    fun hola() {
+        Log.i("ola","hola")
+    }
+}
--- app/src/main/java/com/example/vetsoft/MainActivity.kt
+++ app/src/main/java/com/example/vetsoft/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.vetsoft
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import com.example.vetsoft.Conex.conx
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val conect =conx()
+        conect.hola()
+    }
+
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+        return super.onCreateView(name, context, attrs)
+
     }
 }
