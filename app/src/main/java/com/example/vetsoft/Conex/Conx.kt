1072292c4d5c1d618b731f6c3f0427af4595466a
LUIS EDUARDO ALVARENGA CLAROS <20220298@ricaldone.edu.sv>
2023-05-27 21:15:52 -0600

    Cambio

--- app/src/main/java/com/example/vetsoft/Conex/Conx.kt
+++ app/src/main/java/com/example/vetsoft/Conex/Conx.kt
@@ -1,9 +1,42 @@
 package com.example.vetsoft.Conex
 
+import android.os.StrictMode
 import android.util.Log
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.SQLException
 
 class conx {
-    fun hola() {
-        Log.i("ola","hola")
+    private val ip="10.0.2.2:1433"
+    //private val ipGerson="10.20.2.63:57430"
+    private val db="MediLife"
+    private val username="userSQL"
+    private val password="pasf2"
+
+    fun dbConn(): Connection?{
+        val policy= StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
+        var conn: Connection?=null
+        val connString:String
+        try{
+            Class.forName("net.sourceforge.jtds.jdbc.Driver").newInstance()
+            connString="jdbc:jtds:sqlserver://" +
+                    "$ip;" +
+                    "databaseName=$db;" +
+                    "user=$username;" +
+                    "password=$password"
+
+            conn= DriverManager.getConnection(connString)
+        }
+        catch (ex: SQLException){
+            Log.e("Error: ",ex.message!!)
+        }
+        catch (ex1:ClassNotFoundException){
+            Log.e("Error: ",ex1.message!!)
+        }
+        catch (ex2:ClassNotFoundException){
+            Log.e("Error: ",ex2.message!!)
+        }
+        return conn
     }
 }
