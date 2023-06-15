1a40cf7b794856374e82780a44a8ebc359d07676
LUIS EDUARDO ALVARENGA CLAROS <20220298@ricaldone.edu.sv>
2023-06-14 21:09:52 -0600

    Cambio

--- app/src/main/java/com/example/vetsoft/ui/house/AgendarCita.kt
+++ app/src/main/java/com/example/vetsoft/ui/house/AgendarCita.kt
@@ -35,7 +35,7 @@ class AgendarCIta : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_agendar_c_ita, container, false)
+        return inflater.inflate(R.layout.fragment_agendar_cita, container, false)
     }
 
     companion object {
