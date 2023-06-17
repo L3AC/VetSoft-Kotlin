a8b9118d911b0683019d72092877ef818afaebce
LUIS EDUARDO ALVARENGA CLAROS <20220298@ricaldone.edu.sv>
2023-06-16 19:18:25 -0600

    Cambio

--- app/src/main/java/com/example/vetsoft/Pcarga.kt
+++ app/src/main/java/com/example/vetsoft/Pcarga.kt
@@ -1,29 +1,22 @@
-package com.example.vetsoft;
+package com.example.vetsoft
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.Handler;
-import android.view.WindowManager;
-
-public class Pcarga extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pcarga);
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(Pcarga.this,MainActivity.class));
-                finish();
-            }
-        }, 3000);
-
+class Pcarga : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pcarga)
+        Handler().postDelayed({
+            startActivity(Intent(this@Pcarga, MainActivity::class.java))
+            finish()
+        }, 3000)
     }
 }
