9c24e7380a69ac3e390cd1a648442b8d1d1c58f6
LUIS EDUARDO ALVARENGA CLAROS <20220298@ricaldone.edu.sv>
2023-06-19 19:25:52 -0600

    Cambio

---
+++ app/src/main/java/com/example/vetsoft/ui/house/MainMascota.kt
@@ -0,0 +1,27 @@
+package com.example.vetsoft.ui.house
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.vetsoft.R
+
+class MainMascota : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main_mascota, container, false)
+    }
+
+}
--- app/src/main/java/com/example/vetsoft/ui/house/houseCliente.kt
+++ app/src/main/java/com/example/vetsoft/ui/house/houseCliente.kt
@@ -5,10 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.navigation.fragment.findNavController
 import com.example.vetsoft.Conex.conx
 import com.example.vetsoft.R
 import com.example.vetsoft.Validation.Validat
-
+lateinit var btnMasc4:ImageButton
+lateinit var btnPnd4:ImageButton
+lateinit var btnHist4:ImageButton
 class houseCliente : Fragment() {
     private var idUs: Int = 0
     private var idCl:Int=0
@@ -27,6 +31,27 @@ class houseCliente : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_house_cliente, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btnMasc4=requireView().findViewById(R.id.btnMasc4)
+        btnPnd4=requireView().findViewById(R.id.btnPnd4)
+        btnHist4=requireView().findViewById(R.id.btnHist4)
+
+        val bundle = Bundle().apply {
+            putInt("idUs", idUs)
+            putInt("idCl", idCl)
+        }
+        btnMasc4.setOnClickListener(){
+            findNavController().navigate(R.id.action_houseCliente_to_mainMascota, bundle)
+        }
+        btnPnd4.setOnClickListener(){
+            findNavController().navigate(R.id.action_houseCliente_to_citasPendientes, bundle)
+        }
+        btnHist4.setOnClickListener(){
+            findNavController().navigate(R.id.action_houseCliente_to_historialCitas, bundle)
+        }
     }
 
 }
