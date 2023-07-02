58f8bf736eec0443f311c59dbea9c68835c705e0
LUIS EDUARDO ALVARENGA CLAROS <20220298@ricaldone.edu.sv>
2023-07-01 23:16:21 -0600

    Cambio

--- app/src/main/java/com/example/vetsoft/Controlador/ui/Home/infoCita.kt
+++ app/src/main/java/com/example/vetsoft/Controlador/ui/Home/infoCita.kt
@@ -1,32 +1,61 @@
 package com.example.vetsoft.Controlador.ui.Home
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.Spinner
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.findNavController
+import com.example.vetsoft.Controlador.ui.Perfil.btnActDP
+import com.example.vetsoft.Controlador.ui.Perfil.btnGuardarDP
+import com.example.vetsoft.Controlador.ui.Perfil.btnNaciDP
+import com.example.vetsoft.Controlador.ui.Perfil.btnVolverDP
+import com.example.vetsoft.Controlador.ui.Perfil.spSexoDP
+import com.example.vetsoft.Controlador.ui.Perfil.txtApellDP
+import com.example.vetsoft.Controlador.ui.Perfil.txtCorreoDP
+import com.example.vetsoft.Controlador.ui.Perfil.txtDuiDP
+import com.example.vetsoft.Controlador.ui.Perfil.txtNaciDP
+import com.example.vetsoft.Controlador.ui.Perfil.txtNombDP
+import com.example.vetsoft.Controlador.ui.Perfil.txtTelDP
+import com.example.vetsoft.Controlador.ui.Perfil.txtUsDP
+import com.example.vetsoft.Controlador.ui.Perfil.txvUsDP
+import com.example.vetsoft.Controlador.validation.Validat
+import com.example.vetsoft.Modelo.conx
 import com.example.vetsoft.R
+import java.sql.PreparedStatement
+import java.sql.ResultSet
+import java.sql.SQLException
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [infoCita.newInstance] factory method to
- * create an instance of this fragment.
- */
+lateinit var txtMascFC: EditText
+lateinit var txtFechaFC: EditText
+lateinit var txtMotivoFC: EditText
+lateinit var txtDoctorFC: EditText
+lateinit var txtNDFC: EditText
+lateinit var txtNCFC: EditText
+lateinit var btnVolverFC: ImageButton
+lateinit var btnInfoDocFC: Button
+lateinit var btnEliminarFC: Button
 class infoCita : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
+    private var idUs: Int = 0
+    private var idCl: Int = 0
+    private var idCit: Int = 0
+    private var conx = conx()
+    private var vali = Validat()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            idUs = arguments?.getInt("idUs")!!
+            idCl = arguments?.getInt("idCl")!!
+            idCit = arguments?.getInt("idCit")!!
         }
     }
 
@@ -38,23 +67,84 @@ class infoCita : Fragment() {
         return inflater.inflate(R.layout.fragment_info_cita, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment infoCita.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            infoCita().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        txtMascFC =requireView().findViewById(R.id.txtMascFC)
+        txtFechaFC =requireView().findViewById(R.id.txtFechaFC)
+        txtMotivoFC =requireView().findViewById(R.id.txtMotivoFC)
+        txtDoctorFC =requireView().findViewById(R.id.txtDoctorFC)
+        txtNDFC =requireView().findViewById(R.id.txtNDFC)
+        txtNCFC =requireView().findViewById(R.id.txtNCFC)
+        btnVolverFC =requireView().findViewById(R.id.btnVolverFC)
+        btnInfoDocFC =requireView().findViewById(R.id.btnInfoDocFC)
+        btnEliminarFC =requireView().findViewById(R.id.btnEliminarFC)
+
+        Habilit(false)
+        val bundle = Bundle().apply {
+            putInt("idUs", idUs)
+            putInt("idCl", idCl)
+            putInt("idCit", idCit)
+        }
+
+        btnVolverFC.setOnClickListener(){
+            findNavController().navigate(R.id.action_infoCita_to_citasPendientes, bundle)
+        }
+        btnInfoDocFC.setOnClickListener(){
+            findNavController().navigate(R.id.action_infoCita_to_citasPendientes, bundle)
+        }
+        btnEliminarFC.setOnClickListener(){
+            EliminarCit();
+        }
     }
+    fun CargarDatos() {
+        try {
+            var cadena: String =
+                "select ci.idCliente,CONCAT(d.nombres,' ',d.apellidos) as Doctor,e.especialidad,FORMAT(fechahora,'dd-MM-yyyy') AS Fecha,\n" +
+                        "FORMAT(fechahora,'hh:mm tt') as Hora, CONCAT(c.nombres,' ',c.apellidos) as Paciente from tbCitas ci,\n" +
+                        "tbClientes c,tbDoctores d,tbEspecialidades e where ci.idCliente=c.idCliente and e.idEspecialidad=d.idEspecialidad \n" +
+                        "and  ci.idDoctor=d.idDoctor and idCita=?;"
+            var st: ResultSet
+            val ps: PreparedStatement = conx.dbConn()?.prepareStatement(cadena)!!
+
+            /*ps.setInt(1, idCita)
+
+            st = ps.executeQuery()
+            st.next()
+            txtMedico.setText(st.getString("Doctor"))
+            txtEsp.setText(st.getString("especialidad"))
+            txtFecha2.setText(st.getString("Fecha"))
+            txtHora2.setText(st.getString("Hora"))
+            txtNombre.setText(st.getString("Paciente"))
+            idCliente = st.getInt("idCliente")*/
+
+
+        } catch (ex: SQLException) {
+            Toast.makeText(context, "Error al mostrar", Toast.LENGTH_SHORT).show()
+        }
+    }
+    fun EliminarCit() {
+        try {
+            var cadena: String =
+                "delete tbCitas  where idCita=?;"
+            var st: ResultSet
+            val ps: PreparedStatement = conx.dbConn()?.prepareStatement(cadena)!!
+            Log.i("elim", idCit.toString())
+            ps.setInt(1, idCit)
+
+            ps.executeUpdate()
+            Toast.makeText(context, "Cita eliminada", Toast.LENGTH_SHORT).show()
+        } catch (ex: SQLException) {
+            Log.i("elim", ex.message.toString())
+            Toast.makeText(context, "Error al eliminar", Toast.LENGTH_SHORT).show()
+        }
+    }
+    fun Habilit(tf: Boolean) {
+        txtMascFC.isEnabled =tf
+        txtFechaFC.isEnabled =tf
+        txtMotivoFC.isEnabled =tf
+        txtDoctorFC.isEnabled =tf
+        txtNDFC.isEnabled =tf
+        txtNCFC.isEnabled =tf
+    }
+
 }
