da39ac7f5fd18befe648cd0a6fdcba74d6f8592b
LUIS EDUARDO ALVARENGA CLAROS <20220298@ricaldone.edu.sv>
2023-07-31 09:05:19 -0600

    Cambio

--- app/src/main/java/com/example/vetsoft/Controlador/ui/Tienda/Productos.kt
+++ app/src/main/java/com/example/vetsoft/Controlador/ui/Tienda/Productos.kt
@@ -2,7 +2,12 @@ package com.example.vetsoft.Controlador.ui.Tienda
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.GestureDetector
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -11,13 +16,20 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.vetsoft.Modelo.conx
 import com.example.vetsoft.R
+import java.sql.PreparedStatement
+import java.sql.ResultSet
+import java.sql.SQLException
+
 class classProd(
-    val id: Int, val nTipoProd: String
+    val id: Int, val nProd: String,val prov:String, val precio:Float,val img:ByteArray
 )
 
 val regProd2 = mutableListOf<classProd>()
@@ -56,25 +68,91 @@ class Productos : Fragment() {
 
         val miAdapter = prodCard(myDataProd2)
         rcProd.adapter = miAdapter
+
+        val bundle = Bundle().apply {
+            putInt("idUs", idUs)
+            putInt("idCl", idCl)
+        }
+        CargarByN()
+
+        btnVolver.setOnClickListener(){
+            findNavController().navigate(R.id.action_catalogoProd_to_mainTienda, bundle)
+        }
+        txtNProd.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                CargarByN()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
+
+    }
+    fun CargarByN() {
+        myDataProd2.clear()
+        regProd2.clear()
+        try {
+            var st: ResultSet
+            var cadena: String = "select * from tbProductos p, tbTipoProductos tp " +
+                    "where p.idTipoProducto=tp.idTipoProducto and Nombre like ? and p.idTipoProducto=?;"
+
+            val ps: PreparedStatement = conx.dbConn()?.prepareStatement(cadena)!!
+            ps.setString(1, '%'+txtNProd.text.toString()+'%')
+            ps.setInt(2, idTipoP)
+            Log.i("sda",'%'+txtNProd.text.toString()+'%')
+            st = ps.executeQuery()
+
+            while (st?.next() == true) {
+                val col1 = st.getInt("idProducto")
+                val col2 = st.getString("Nombre")
+                val col3 = st.getString("Proveedor")
+                val col4 = st.getFloat("Precio")
+                val col5 = st.getBytes("img")
+
+                regProd2.add(classProd(col1, col2,col3,col4,col5))
+                val newElement = "$col2"
+                myDataProd2.add(newElement)
+
+                val miAdapter = prodCard(myDataProd2)
+                rcProd.adapter = miAdapter
+            }
+
+        } catch (ex: SQLException) {
+            Log.i("ol", ex.message.toString())
+            Toast.makeText(context, "Error al cargar cita N", Toast.LENGTH_SHORT).show()
+        }
     }
     class prodCard(private val Datos: MutableList<String>/*,private val btnClick:(Int)->Unit*/) :
         RecyclerView.Adapter<prodCard.MyViewHolder>() {
 
         class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-            val txvTipoP: TextView = view.findViewById(R.id.txvNomb)
+            val txvNomb: TextView = view.findViewById(R.id.txvNomb)
+            val txvMarca: TextView = view.findViewById(R.id.txvMarca)
+            val txvPrecio: TextView = view.findViewById(R.id.txvPrecio)
+            val imgP: ImageView = view.findViewById(R.id.imgP)
         }
 
         @SuppressLint("MissingInflatedId")
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
             val vista =
-                LayoutInflater.from(parent.context).inflate(R.layout.tipoprod, parent, false)
+                LayoutInflater.from(parent.context).inflate(R.layout.products, parent, false)
             return MyViewHolder(vista)
         }
 
         override fun getItemCount() = Datos.size
         override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-            val itm = regProd[position]
-            holder.txvTipoP.text = itm.nTipoProd
+            val itm = regProd2[position]
+            holder.txvNomb.text = itm.nProd
+            holder.txvMarca.text=itm.prov
+            holder.txvPrecio.text=itm.precio.toString()
+            if (itm.img != null) {
+                val bitmap: Bitmap = BitmapFactory.decodeByteArray(itm.img, 0, itm.img.size)
+                holder.imgP.setImageBitmap(bitmap)
+            }
+
             //Reemplazamos la imagen
             //  holder.imageView.setImageResource(Imagenes[position])
         }
